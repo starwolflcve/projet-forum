d658f65d529ceb1d815e2fc756998c1a1124f5b4
quentinbardou21-max <quentin.bardou21@gmail.com>
2026-05-06 09:45:50 +0200

    Add activity model and activity page handler

---
+++ internal/handlers/activity_handler.go
@@ -0,0 +1,43 @@
+package handlers
+
+import (
+	"html/template"
+	"net/http"
+
+	"forum/internal/database"
+	"forum/internal/models"
+)
+
+type ActivityPageData struct {
+	Title     string
+	UserID    int
+	Posts     []models.ActivityItem
+	Reactions []models.ActivityItem
+	Comments  []models.ActivityItem
+}
+
+func ActivityHandler(db *sql.DB, tmpl *template.Template) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		userID := 1 // temporaire, à remplacer plus tard par l'utilisateur connecté
+
+		activityData, err := database.ListActivityByUserID(db, userID)
+		if err != nil {
+			http.Error(w, "Erreur lors du chargement de l'activité", http.StatusInternalServerError)
+			return
+		}
+
+		data := ActivityPageData{
+			Title:     "Mon activité",
+			UserID:    userID,
+			Posts:     activityData["posts"],
+			Reactions: activityData["reactions"],
+			Comments:  activityData["comments"],
+		}
+
+		err = tmpl.ExecuteTemplate(w, "activity.html", data)
+		if err != nil {
+			http.Error(w, "Erreur lors de l'affichage de la page", http.StatusInternalServerError)
+			return
+		}
+	}
+}
