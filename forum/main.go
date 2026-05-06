a9260db2dd8c9fc1008a770a6b03bdc6903a88a2
quentinbardou21-max <quentin.bardou21@gmail.com>
2026-05-06 10:24:26 +0200

    chore: add database init and wire routes

--- forum/main.go
+++ forum/main.go
@@ -1,4 +1,32 @@
 package main
 
+import (
+	"html/template"
+	"log"
+	"net/http"
+
+	"forum/internal/database"
+	"forum/internal/handlers"
+)
+
 func main() {
+	db, err := database.InitDB("db/forum.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	tmpl := template.Must(template.ParseGlob("web/templates/pages/*.html"))
+
+	http.HandleFunc("/activity", handlers.ActivityHandler(db, tmpl))
+	http.HandleFunc("/notifications", handlers.NotificationsHandler(db, tmpl))
+	http.HandleFunc("/notifications/read", handlers.MarkNotificationAsReadHandler(db))
+
+	http.HandleFunc("/moderation", handlers.ModerationDashboardHandler(db, tmpl))
+	http.HandleFunc("/moderation/report", handlers.ReportContentHandler(db))
+	http.HandleFunc("/moderation/approve", handlers.ApproveReportHandler(db))
+	http.HandleFunc("/moderation/reject", handlers.RejectReportHandler(db))
+
+	log.Println("Server started on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
