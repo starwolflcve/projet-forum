07511e78e225c69c411705086071573a1ca98558
Quentin Lacave <quentin.lacave@ynov.com>
2026-05-11 12:02:06 +0200

    securisation reseaux

---
+++ internal/security/headers.go
@@ -0,0 +1,13 @@
+package security
+
+import "net/http"
+
+func SecurityHeadersMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("X-XSS-Protection", "1; mode=block")
+		w.Header().Set("X-Frame-Options", "DENY")
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.Header().Set("Strict-Transport-Security", "max-age=31536000")
+		next.ServeHTTP(w, r)
+	})
+}
